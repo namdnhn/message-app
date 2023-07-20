b45d65b56a3b328df161eb3c86a4b6a326a7d692
namdnhn <donhuhoangnam.work@gmail.com>
2023-07-20 09:44:49 +0700

    refactor: change to composition API

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
+  <div class="custom-background">
     <h1>Message</h1>
     <div class="select-platform-and-sender">
-      <select v-model="selectedPlatform">
+      <label>Select platform: </label>
+      <select class="custom-select" v-model="selectedPlatform">
         <option v-for="platform in platforms" :key="platform" :value="platform">
           {{ platform }}
         </option>
       </select>
 
-      <select v-model="selectedSender">
+      <label> Select User: </label>
+      <select class="custom-select" v-model="selectedSender">
         <option
           v-for="sender in filterSendersByPlatforms"
           :key="sender"
@@ -19,12 +21,18 @@
       </select>
     </div>
     <div class="receiver-url">
-      <input type="text" :value="receiverURL" placeholder="ReceiverURL" />
+      <label>Receiver URL: </label>
+      <input
+        class="custom-textbox"
+        type="text"
+        :value="receiverURL"
+        placeholder="ReceiverURL"
+      />
     </div>
     <div class="message">
       <h3>Message</h3>
       <div class="select-template">
-        <select v-model="selectedTemplate">
+        <select class="custom-select" v-model="selectedTemplate">
           <option
             v-for="template in templates"
             :key="template"
@@ -37,7 +45,11 @@
         <button @click="createAndEditTemplate">Create and Edit Template</button>
       </div>
       <div class="message-box">
-        <textarea v-model="message" placeholder="Message"></textarea>
+        <textarea
+          class="custom-textbox"
+          v-model="message"
+          placeholder="Message"
+        ></textarea>
       </div>
     </div>
 
@@ -71,78 +83,96 @@
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
+import { computed, reactive, ref } from "vue";
 
 export default {
-  data() {
-    return {
-      platforms: ["Linkedin", "Facebook", "Zalo"],
-      senders: {
-        Linkedin: ["Nguyen Van A", "Nguyen Van D"],
-        Facebook: ["Nguyen Van B"],
-        Zalo: ["Nguyen Van C"],
-      } as Record<string, string[]>,
-      templates: ["template1", "template2"],
-      selectedPlatform: "",
-      selectedSender: "",
-      receiverURL: "",
-      selectedTemplate: "",
-      message: "",
-      targetSettings: {
-        changedPosition: false,
-        alreadyConnected: false,
-        followCandidate: false,
-      },
-      targetSettingsMessages: {
-        changedPosition:
-          "Do not send message if current company or position is changed",
-        alreadyConnected: "Do not send to people you've already connected with",
-        followCandidate: "Follow candidate when sending invite message",
-      },
-      showPreviewModel: false,
-      previewData: "",
+  setup() {
+    const platforms = ref(["Linkedin", "Facebook", "Zalo"]);
+    const selectedPlatform = ref("");
+
+    const senders = reactive<Record<string, string[]>>({
+      Linkedin: ["Nguyen Van A", "Nguyen Van D"],
+      Facebook: ["Nguyen Van B"],
+      Zalo: ["Nguyen Van C"],
+    });
+    const selectedSender = ref("");
+    const filterSendersByPlatforms = computed(() => {
+      return senders[selectedPlatform.value] || [];
+    });
+
+    const templates = ref(["template1", "template2"]);
+    const selectedTemplate = ref("");
+    const createAndEditTemplate = () => {};
+
+    const receiverURL = ref("");
+    const message = ref("");
+
+    const targetSettings = reactive({
+      changedPosition: false,
+      alreadyConnected: false,
+      followCandidate: false,
+    });
+    const targetSettingsMessages = {
+      changedPosition:
+        "Do not send message if current company or position is changed",
+      alreadyConnected: "Do not send to people you've already connected with",
+      followCandidate: "Follow candidate when sending invite message",
     };
-  },
-  computed: {
-    filterSendersByPlatforms() {
-      return this.senders[this.selectedPlatform] || [];
-    },
-  },
-  methods: {
-    createAndEditTemplate() {},
-    preview() {
+    const previewData = ref('');
+    const showPreviewModel = ref(false);
+    const preview = () => {
       const previewModel = {
-        platform: this.selectedPlatform,
-        sender: this.selectedSender,
-        receiverURL: this.receiverURL,
-        template: this.selectedTemplate,
-        message: this.message,
-        targetSettings: this.targetSettings,
+        platform: selectedPlatform,
+        sender: selectedSender,
+        receiverURL: receiverURL,
+        template: selectedTemplate,
+        message: message,
+        targetSettings: targetSettings,
       };
-      this.previewData = JSON.stringify(previewModel);
-      this.showPreviewModel = true;
-    },
-    closeModel() {
-      this.showPreviewModel = false;
-    },
-    send() {
+      previewData.value = JSON.stringify(previewModel);
+      showPreviewModel.value = true;
+    };
+    const closeModel = () => {
+      showPreviewModel.value = false;
+    }
+    const send = () => {
       const sendMessage = {
-        platform: this.selectedPlatform,
-        sender: this.selectedSender,
-        receiverURL: this.receiverURL,
-        template: this.selectedTemplate,
-        message: this.message,
-        targetSettings: this.targetSettings,
+        platform: selectedPlatform,
+        sender: selectedSender,
+        receiverURL: receiverURL,
+        template: selectedTemplate,
+        message: message,
+        targetSettings: targetSettings,
       };
-      this.selectedPlatform = "";
-      this.selectedSender = "";
-      this.receiverURL = "";
-      this.selectedTemplate = "";
-      this.message = "";
-      this.targetSettings.alreadyConnected = false;
-      this.targetSettings.changedPosition = false;
-      this.targetSettings.followCandidate = false;
-    },
+      selectedPlatform.value = "";
+      selectedSender.value = "";
+      receiverURL.value = "";
+      selectedTemplate.value = "";
+      message.value = "";
+      targetSettings.alreadyConnected = false;
+      targetSettings.changedPosition = false;
+      targetSettings.followCandidate = false;
+    }
+
+    return {
+      platforms,
+      senders,
+      templates,
+      selectedPlatform,
+      selectedSender,
+      receiverURL,
+      selectedTemplate,
+      message,
+      targetSettings,
+      targetSettingsMessages,
+      showPreviewModel,
+      previewData,
+      filterSendersByPlatforms,
+      createAndEditTemplate,
+      preview,
+      closeModel,
+      send,
+    };
   },
 };
 </script>
@@ -151,5 +181,13 @@ export default {
 .checkbox-message {
   display: flex;
   flex-direction: column;
+}
+.custom-select {
+  width: 35%;
+  height: 30px;
+}
+.custom-textbox {
+  width: 90%;
+  height: 100px;
 }
 </style>
