1522615c1a96845a414bcaa67f13665e0594e643
namdnhn <donhuhoangnam.work@gmail.com>
2023-07-25 08:40:03 +0700

    refactor: connect to database

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,6 @@
-from typing import Union
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import asyncpg
 
 app = FastAPI()
 
@@ -13,26 +12,50 @@ app.add_middleware(
     allow_headers=["*", "sentry-trace", "baggage"],
 )
 
-platforms = ["Linkedin", "Facebook", "Zalo"]
+DATABASE_URL = "postgresql://postgres:12341234@localhost:5432/messageapp"
+
+
+async def create_connection_pool():
+    return await asyncpg.create_pool(DATABASE_URL)
+
+
+@app.on_event("startup")
+async def startup_event():
+    app.state.pool = await create_connection_pool()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await app.state.pool.close()
+
 
 @app.get("/platforms")
 async def read_platforms():
-    return platforms
+    async with app.state.pool.acquire() as connection:
+        query = "SELECT * FROM platform"
+        rows = await connection.fetch(query)
+        return [row["name"] for row in rows]
 
-senders = {
-    "Linkedin": ["Nguyen Van A", "Nguyen Van D"],
-    "Facebook": ["Nguyen Van B"],
-    "Zalo": ["Nguyen Van C"],
-}
 
 @app.get("/senders")
 async def get_senders():
-    return senders
+    async with app.state.pool.acquire() as connection:
+        query = "SELECT s.name as s_name, p.name as p_name FROM sender s JOIN platform p ON p.id = s.platform;"
+        rows = await connection.fetch(query)
+        result = {}
+        for row in rows:
+            platform = row["p_name"]
+            sender = row["s_name"]
+            if platform not in result:
+                result[platform] = [sender]
+            else:
+                result[platform].append(sender)
+        return result
 
-
-templates = ['template1', 'template2']
 
 @app.get("/templates")
 async def get_templates():
-    return templates
-
+    async with app.state.pool.acquire() as connection:
+        query = "SELECT * FROM template"
+        rows = await connection.fetch(query)
+        return [row["name"] for row in rows]
